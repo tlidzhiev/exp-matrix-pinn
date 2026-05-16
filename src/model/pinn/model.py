e8506bfaa3ee4360774680dc66b0682082ea9e96
tlidzhiev <trlidzhiev@gmail.com>
2026-05-16 16:04:57 +0300

    add anzatz

--- src/model/pinn/model.py
+++ src/model/pinn/model.py
@@ -16,8 +16,12 @@ class PINN(BaseModel):
         t_domain: tuple[float, float],
         solver_params: dict[str, Any],
         solver_name: _SolverName = 'baseline',
+        ansatz_type: Literal['linear', 'exp'] = 'linear',
     ) -> None:
         super().__init__()
+        self.ansatz_type = ansatz_type
+        if self.ansatz_type == 'exp':
+            self.tau = torch.nn.Parameter(torch.tensor([1.0], dtype=torch.float32))
         self.solver = get_solver(solver_name, {'k': k, 'n': n, **solver_params})
         self.register_buffer('t_min', torch.tensor(t_domain[0], dtype=torch.float32))
         self.register_buffer('t_max', torch.tensor(t_domain[1], dtype=torch.float32))
@@ -33,7 +37,10 @@ class PINN(BaseModel):
         x_matrix = torch.cat([x, u0.unsqueeze(1)], dim=1)
         t_offset = t - self.t_min
         delta = self.solver(t_norm, x_matrix)
-        ut_raw = u0 + t_offset * delta
+        if self.ansatz_type == 'linear':
+            ut_raw = u0 + t_offset * delta
+        else:
+            ut_raw = u0 + (1 - torch.exp(-t_offset / (self.tau.abs() + 1e-6))) * delta
         u0_norm = u0.norm(dim=-1, keepdim=True)
         ut_norm = ut_raw.norm(dim=-1, keepdim=True) + 1e-8
         ut = (ut_raw / ut_norm) * u0_norm
